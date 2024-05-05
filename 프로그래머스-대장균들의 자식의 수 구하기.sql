230c341c1990a5f216ee09c0b8b6a3ee4c65d36a
eunyeongkimm <101814174+eunyeongkimm@users.noreply.github.com>
2024-05-05 19:52:31 +0900

    Update 프로그래머스-대장균들의 자식의 수 구하기.sql

--- 프로그래머스-대장균들의 자식의 수 구하기.sql
+++ 프로그래머스-대장균들의 자식의 수 구하기.sql
@@ -1,6 +1,6 @@
---left join + null값은 0으로 채우기
+--left join 활용
 
-SELECT p.id, IFNULL(COUNT(c.parent_id),0) AS child_count
+SELECT p.id, COUNT(c.parent_id) AS child_count
 FROM ecoli_data p
 LEFT JOIN ecoli_data c
 ON p.id = c.parent_id
