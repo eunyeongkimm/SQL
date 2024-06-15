ef24e0de3440a49eb2015fdfc9132befa3e0626c
eunyeongkimm <101814174+eunyeongkimm@users.noreply.github.com>
2024-06-15 14:40:01 +0900

    Update 550. Game Play Analysis IV.sql

--- 리트코드/550. Game Play Analysis IV.sql
+++ 리트코드/550. Game Play Analysis IV.sql
@@ -13,3 +13,20 @@ WHERE
         FROM activity
         GROUP BY player_id
     );
+
+
+-- 다른풀이
+
+SELECT ROUND(SUM(CASE WHEN DATEDIFF(aa.second_event, aa.first_event) = 1 THEN 1.0 ELSE 0.0 END) / count(aa.player_id),2) as fraction
+FROM ( 
+ SELECT player_id, first_event, all_event as second_event
+ FROM (
+    SELECT 
+        player_id,
+        MIN(event_date) OVER (PARTITION BY player_id) AS first_event,
+        event_date AS all_event,
+        ROW_NUMBER() OVER (PARTITION BY player_id ORDER BY event_date) AS event_rank
+    FROM lt_game.activity
+ ) event_all
+ WHERE event_rank = 2
+) aa
