8207a0ba058595761266a83ab2e9ffbe5c381a62
eunyeongkimm <101814174+eunyeongkimm@users.noreply.github.com>
2024-06-15 14:40:29 +0900

    Update 550. Game Play Analysis IV.sql

--- 리트코드/550. Game Play Analysis IV.sql
+++ 리트코드/550. Game Play Analysis IV.sql
@@ -1,9 +1,6 @@
-SELECT 
-    ROUND(SUM(CASE WHEN a2.event_date IS NOT NULL THEN 1 ELSE 0 END) / COUNT(DISTINCT a1.player_id), 2) AS fraction
-FROM 
-    activity a1
-LEFT JOIN 
-    activity a2 
+SELECT ROUND(SUM(CASE WHEN a2.event_date IS NOT NULL THEN 1 ELSE 0 END) / COUNT(DISTINCT a1.player_id), 2) AS fraction
+FROM activity a1
+LEFT JOIN activity a2 
 ON 
     a1.player_id = a2.player_id 
     AND a2.event_date = a1.event_date + INTERVAL 1 DAY
