d695f96d11e3badaf88b9a83f0b8c616aecbf9a6
eunyeongkimm <101814174+eunyeongkimm@users.noreply.github.com>
2024-06-16 15:25:23 +0900

    Create 1045. Customers Who Bought All Products.sql

---
+++ 리트코드/1045. Customers Who Bought All Products.sql
@@ -0,0 +1,10 @@
+SELECT customer_id
+FROM Customer c
+LEFT JOIN Product p
+ON c.product_key = p.product_key
+GROUP BY customer_id
+HAVING COUNT(DISTINCT(c.product_key)) 
+  = (
+        SELECT COUNT(*)
+        FROM Product
+     )
